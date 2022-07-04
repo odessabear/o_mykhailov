5940ed1c4e5d3fb232994262bbefa18be9a17257
odessabear <bearodessa@gmail.com>
2022-07-05 02:33:08 +0300

    homework_3, start basic project structure

---
+++ web-provider/src/main/java/com/epam/spring/webprovider/service/model/Tariff.java
@@ -0,0 +1,23 @@
+package com.epam.spring.webprovider.service.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class Tariff {
+
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private String imageLink;
+
+    private BigDecimal price;
+
+    private Category category;
+}
