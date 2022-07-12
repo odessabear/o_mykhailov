ad1e121e28ab09e09858a60a354e45b12bdba23c
odessabear <bearodessa@gmail.com>
2022-07-12 17:23:02 +0300

    add missed annotation

--- homework_2/src/main/java/com/epam/spring/homework2/config/SecondBeansConfig.java
+++ homework_2/src/main/java/com/epam/spring/homework2/config/SecondBeansConfig.java
@@ -3,7 +3,9 @@ package com.epam.spring.homework2.config;
 import com.epam.spring.homework2.beans.MyCustomBeanPostProcessor;
 import com.epam.spring.homework2.beans.OtherBeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SecondBeansConfig {
     @Bean
     public MyCustomBeanPostProcessor myCustomBeanPostProcessor() {
